e64b6afbf14a70f7789f8b3b2ddcbb094eccaa12
Quick <quickness@gmail.com>
2018-12-25 00:56:09 -0600

    Fixed issue with occasional error popup

--- Razor/Network/Handlers.cs
+++ Razor/Network/Handlers.cs
@@ -1942,9 +1942,9 @@ namespace Assistant
 
                 if (Config.GetBool("ShowContainerLabels") && ser.IsItem)
                 {
-                     Item item = World.FindItem(ser);
+                    Item item = World.FindItem(ser);
                     
-                    if (!item.IsContainer)
+                    if (item == null || !item.IsContainer)
                         return;
 
                     foreach (ContainerLabels.ContainerLabel label in ContainerLabels.ContainerLabelList)
